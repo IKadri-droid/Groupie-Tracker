b5688b661f7d3dfbc96b9fba9e17f4fc268b1bb3
Thomaslgrn <thomasleguern5@gmail.com>
2026-01-17 00:45:19 +0100

    new components for admin page

---
+++ src/routes/admin/components/AdminArtistCard.tsx
@@ -0,0 +1,47 @@
+import { ArtistCard } from "@/features/artists";
+import type { Artist } from "@/features/artists";
+import { Button } from "@/shared/components/ui/button";
+import { Pencil, Trash2 } from "lucide-react";
+import { useDeleteArtist } from "@/features/artists";
+
+interface Prop {
+  artist: Artist;
+}
+
+export default function AdminArtistCard({ artist }: Prop) {
+  const deleteArtistMutation = useDeleteArtist();
+  return (
+    <div className="relative group ">
+      <ArtistCard
+        key={artist.id}
+        artist={artist}
+        disableHover={true}
+        onClick={() => {}}
+      />
+      <div className="absolute  top-2 right-2 flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity ">
+        <Button
+          size="icon"
+          variant="secondary"
+          onClick={(e) => {
+            e.stopPropagation(); // Empêche le onClick de la carte
+            // TODO: Modifier
+          }}
+        >
+          {" "}
+          <Pencil className="h-4 w-4" />
+        </Button>
+
+        <Button
+          size="icon"
+          variant="destructive"
+          onClick={(e) => {
+            e.stopPropagation();
+            deleteArtistMutation.mutate(artist.id);
+          }}
+        >
+          <Trash2 className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
+  );
+}
