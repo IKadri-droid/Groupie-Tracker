f4b2388ad01fdba988fc13ab7d1e5499ab0881ae
Thomaslgrn <thomasleguern5@gmail.com>
2026-01-16 11:55:23 +0100

    new components artist card

--- src/features/artists/index.ts
+++ src/features/artists/index.ts
@@ -1,4 +1,5 @@
 // Public exports for Artists feature
 export { default as ArtistDialog } from './components/ArtistDialog'
+export { default as ArtistCard } from './components/ArtistCard'
 export { useArtists } from './hooks/useArtists'
 export * from './types/artist.types'
