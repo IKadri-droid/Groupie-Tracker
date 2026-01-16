d460136dd679c6a66c3e477fcec61e4e24ba7547
Thomaslgrn <thomasleguern5@gmail.com>
2026-01-16 11:54:28 +0100

    delete concert from ticketmaster

--- src/features/concerts/index.ts
+++
@@ -1,4 +0,0 @@
-// Public exports for Concerts feature
-export { ConcertList } from './components/ConcertList'
-export { useConcerts } from './hooks/useConcerts'
-export * from './types/ticketmaster.types'
