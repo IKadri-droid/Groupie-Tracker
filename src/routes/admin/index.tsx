e20ac559803a90b385994011dadfb0d0180579c6
Thomaslgrn <thomasleguern5@gmail.com>
2026-01-17 00:44:26 +0100

    move and rename admin.tsx for a better architecture

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -1,28 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useState, useEffect, useRef } from "react";
-import {
-  useArtists,
-  ArtistCard,
-  type Artist,
-  CreateArtistDialog,
-} from "@/features/artists";
+import { useState } from "react";
+import { useArtists, CreateArtistDialog } from "@/features/artists";
 import { Button } from "@/shared/components/ui/button";
 import { Plus } from "lucide-react";
 import { Card, CardContent } from "@/shared/components/ui/card";
+import AdminArtistCard from "./components/AdminArtistCard";
 
-export const Route = createFileRoute("/admin")({
+export const Route = createFileRoute("/admin/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [currentColor, setCurrentColor] = useState<string | null>(null);
   const { data: artists = [] } = useArtists();
   const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false);
-  const [selectedArtist, setSelectedArtist] = useState<Artist | null>(null);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  {
-    /* State pour stocker les données du formulaire de création d'artistes*/
-  }
 
   return (
     <div>
@@ -37,34 +27,23 @@ function RouteComponent() {
         </div>
       </div>
 
-      <div
-        className="mt-24 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4"
-        onMouseLeave={() => setCurrentColor(null)}
-      >
+      <div className="mt-24 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
         {artists.map((artist) => (
-          <ArtistCard
-            key={artist.id}
-            artist={artist}
-            onMouseEnter={() => setCurrentColor(artist.color ?? null)}
-            onClick={() => {
-              setSelectedArtist(artist);
-              setIsDialogOpen(true);
-            }}
-          />
+          <AdminArtistCard artist={artist}></AdminArtistCard>
         ))}
         <Card
           className="group relative h-96 overflow-hidden rounded-2xl cursor-pointer shadow-2xl transition-all hover:-translate-y-2 border-0 bg-transparent p-0"
           onClick={() => setIsCreateDialogOpen(true)}
         >
           <CardContent className="h-full w-full flex items-center justify-center p-0">
-            <Plus className="w-16 h-16 text-black-400"></Plus>
+            <Plus className="w-16 h-16 text-black-400" />
           </CardContent>
         </Card>
       </div>
       <CreateArtistDialog
         open={isCreateDialogOpen}
         onOpenChange={setIsCreateDialogOpen}
-      ></CreateArtistDialog>
+      />
     </div>
   );
 }
