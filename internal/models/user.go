f973ad0cc84b20c18a433bb714ea88a6b57b3536
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-17 19:06:56 +0100

    add table for creation users

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,11 +1,24 @@
 package models
 
-import "time"
+import (
+	"groupie/internal/config"
+	"time"
+)
 
 type User struct {
-	ID           int       `json:"id"`
-	Email        string    `json:"email"`
-	Username     string    `json:"username"`
-	Password     string    `json:"-"`
-	creationDate time.Time `json:"Date de création"`
+	ID       int       `json:"id"`
+	Email    string    `json:"email"`
+	Username string    `json:"username"`
+	Password string    `json:"-"`
+	CreateAt time.Time `json:"Date de création"`
+}
+
+func CreateUser(user *User) error {
+	query := "INSERT INTO users (email, username, password) Values ($1, $2, $3)"
+	_, err := config.DB.Exec(query, user.Email, user.Username, user.Password)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
