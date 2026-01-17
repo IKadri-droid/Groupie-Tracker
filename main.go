46e9723025961bc0d6a73c689146cec4a6712872
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-17 20:12:10 +0100

    add handlefunc for register api

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 	http.HandleFunc("/api/deezer/search", api.HandleDeezerSearch)
 	http.HandleFunc("/api/deezer/albums", api.HandleDeezerAlbums)
 	http.HandleFunc("/api/deezer/top-tracks", api.HandleDeezerTopTracks)
+	http.HandleFunc("/api/register", api.HandleRegister)
 
 	port := ":8080"
 	fmt.Println("🚀 REST API Server started on http://localhost" + port)
