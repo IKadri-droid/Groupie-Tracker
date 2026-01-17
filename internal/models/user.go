421e7b5877cb141358f7e0d64be12d334b369f5a
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-17 23:06:57 +0100

    add GetUserByEmail and updta User tags for JSON map

--- internal/models/user.go
+++ internal/models/user.go
@@ -9,7 +9,7 @@ type User struct {
 	ID       int       `json:"id"`
 	Email    string    `json:"email"`
 	Username string    `json:"username"`
-	Password string    `json:"-"`
+	Password string    `json:"password"`
 	CreateAt time.Time `json:"Date de création"`
 }
 
@@ -21,4 +21,25 @@ func CreateUser(user *User) error {
 		return err
 	}
 	return nil
+}
+
+func GetUserByEmail(email string) (*User, error) {
+	var user User
+
+	query := "SELECT id, email, username, password, created_at FROM users WHERE email = $1"
+	// 2. On exécute et on scanne une SEULE fois
+	// On met tous les champs dans l'ordre du SELECT
+	err := config.DB.QueryRow(query, email).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Username,
+		&user.Password,
+		&user.CreateAt,
+	)
+	// 3. Il faut ABSOLUMENT vérifier l'erreur ici
+	if err != nil {
+		return nil, err // Si on ne trouve rien, on renvoie "vide" et l'erreur
+	}
+	// 4. On renvoie l'adresse de l'utilisateur trouvé
+	return &user, nil
 }
