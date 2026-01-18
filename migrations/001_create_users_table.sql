43840ef2eef14410bffd778fffc05c20d3225ee3
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-18 15:43:29 +0100

    update role with migrations

--- migrations/001_create_users_table.sql
+++ migrations/001_create_users_table.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT UNIQUE NOT NULL,         -- UNIQUE = pas deux fois le même mail
     username TEXT,                      -- Le pseudo
     password TEXT NOT NULL,             -- On stockera le hash ici (c'est du texte)
-    created_at TIMESTAMP DEFAULT NOW()  -- La date de création, remplie automatiquement à "maintenant"
-    role TEXT DEFAULT 'user'            -- Par défaut, tout le monde est un utilisateur normal
-);git 
+    created_at TIMESTAMP DEFAULT NOW(), -- Virgule ici pour passer à la suite
+    role TEXT DEFAULT 'user'            -- Pas de virgule ici, c'est le dernier ingrédient
+);
+
+UPDATE users SET role = 'admin' WHERE email = 'AdminGroupie@groupie.com';
