b2039033d2992f1536ef2e8512cb579090f86d01
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-23 11:43:32 +0100

    add model for concert

--- internal/models/artist.go
+++ internal/models/artist.go
@@ -12,10 +12,15 @@ type Artist struct {
 }
 
 type Concert struct {
-	ID        int     `json:"id"`
-	ArtistID  int     `json:"artist_id"`
-	Location  string  `json:"location"`  // ex: "Paris, France"
-	Date      string  `json:"date"`      // Pour l'instant string, ou time.Time si tu veux
-	Latitude  float64 `json:"latitude"`  // Pour le globe
-	Longitude float64 `json:"longitude"` // Pour le globe
+	ID             int     `json:"id"`
+	ArtistID       int     `json:"artist_id"`
+	Location       string  `json:"location"`  // ex: "Paris, France"
+	Date           string  `json:"date"`      // Pour l'instant string, ou time.Time si tu veux
+	Latitude       float64 `json:"latitude"`  // Pour le globe
+	Longitude      float64 `json:"longitude"` // Pour le globe
+	Time           string  `json:"time"`
+	Venue          string  `json:"venue"`
+	Price          string  `json:"price"`
+	AvailableSeats int     `json:"available_seats"`
+	TicketURL      string  `json:"ticket_url"`
 }
