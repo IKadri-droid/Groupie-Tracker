0594940c84c72d8e27e89b35905bb601bdea5451
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-25 21:09:50 +0100

    add hundler for UserHistory and Favorite setting

--- main.go
+++ main.go
@@ -31,6 +31,16 @@ func main() {
 	http.HandleFunc("/api/register", api.HandleRegister)
 	http.HandleFunc("/api/create-checkout-session", api.HandleCreateCheckoutSession)
 	http.HandleFunc("/api/confirm-payment", api.HandlePaymentConfirm)
+	http.HandleFunc("/api/history", api.GetUserHistory)
+	http.HandleFunc("/api/favorites", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet || r.Method == "OPTIONS" {
+			api.HandleGetFavorites(w, r)
+		} else if r.Method == http.MethodPost {
+			api.HandleAddFavorite(w, r)
+		} else {
+			http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+		}
+	})
 
 	port := ":8080"
 	fmt.Println("🚀 REST API Server started on http://localhost" + port)
