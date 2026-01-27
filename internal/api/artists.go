ac503db39eef66dca7e391de1ca1ed047e4bb10a
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-27 11:21:42 +0100

    update for err artiste model and api

--- internal/api/artists.go
+++ internal/api/artists.go
@@ -188,7 +188,11 @@ func deleteArtist(w http.ResponseWriter, r *http.Request, idStr string) {
 func getConcertsByArtistID(artistID int) ([]models.Concert, error) {
 	query := `
 		SELECT id, artist_id, location, date, latitude, longitude, image_concert,
+<<<<<<< Updated upstream
 		       venue, price, available_seats
+=======
+		       venue, price, available_seats 
+>>>>>>> Stashed changes
 		FROM concerts 
 		WHERE artist_id = $1`
 
@@ -206,6 +210,7 @@ func getConcertsByArtistID(artistID int) ([]models.Concert, error) {
 		var availableSeats sql.NullInt32
 
 		err := rows.Scan(
+<<<<<<< Updated upstream
 			&c.ID,
 			&c.ArtistID,
 			&c.Location,
@@ -216,6 +221,9 @@ func getConcertsByArtistID(artistID int) ([]models.Concert, error) {
 			&venue,
 			&price,
 			&availableSeats,
+=======
+			&c.ID, &c.ArtistID, &c.Location, &dateStr, &c.Latitude, &c.Longitude, &c.ConcertImage, &venue, &price, &availableSeats,
+>>>>>>> Stashed changes
 		)
 		if err != nil {
 			continue
