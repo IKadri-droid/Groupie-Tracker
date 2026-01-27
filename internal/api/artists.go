bc6316b7adfa6be4ef038f10925a452af63834ea
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-27 11:33:32 +0100

    clear function err

--- internal/api/artists.go
+++ internal/api/artists.go
@@ -184,17 +184,21 @@ func deleteArtist(w http.ResponseWriter, r *http.Request, idStr string) {
 	json.NewEncoder(w).Encode(map[string]string{"message": "Artiste supprimé"})
 }
 
-// Fonction utilitaire pour récupérer les concerts d'un artiste
 func getConcertsByArtistID(artistID int) ([]models.Concert, error) {
 	query := `
 		SELECT id, artist_id, location, date, latitude, longitude, image_concert,
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 		       venue, price, available_seats
 		SELECT id, artist_id, location, date, latitude, longitude, image_concert,
 		       venue, price, available_seats
 =======
-		       venue, price, available_seats 
+=======
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+		       venue, price, available_seats 
 		FROM concerts 
 		WHERE artist_id = $1`
 
@@ -212,8 +216,16 @@ func getConcertsByArtistID(artistID int) ([]models.Concert, error) {
 		var availableSeats sql.NullInt32
 
 		err := rows.Scan(
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 			&c.ID, &c.ArtistID, &c.Location, &dateStr, &c.Latitude, &c.Longitude, &concertImg,
 			&venue, &price, &availableSeats,
+=======
+			&c.ID, &c.ArtistID, &c.Location, &dateStr, &c.Latitude, &c.Longitude, &concertImg, &venue, &price, &availableSeats,
+>>>>>>> Stashed changes
+=======
+			&c.ID, &c.ArtistID, &c.Location, &dateStr, &c.Latitude, &c.Longitude, &concertImg, &venue, &price, &availableSeats,
+>>>>>>> Stashed changes
 		)
 		if err != nil {
 			continue
