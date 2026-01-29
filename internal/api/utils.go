093bdaede2723feaf8fb741acda4511c860ff11a
ikadri-droid <229955375+IKadri-droid@users.noreply.github.com>
2026-01-29 17:11:24 +0100

    vérification admin

---
+++ internal/api/utils.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"groupie/internal/services"
+	"net/http"
+	"strings"
+)
+
+func VerifAdmin(r *http.Request) bool {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return false
+	}
+
+	tokenString := strings.TrimPrefix(authHeader, "Bearer")
+
+	claims, err := services.ValidateToken(tokenString)
+	if err != nil {
+		return false
+	}
+
+	role, ok := claims["role"].(string)
+	return ok && role == "admin"
+}
