393732731ecc3f4d0cc7de756e2bc01adb992f10
Thomaslgrn <thomasleguern5@gmail.com>
2026-02-11 20:23:10 +0100

    fix bug ios

---
+++ capacitor.config 2.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.groupie.tracker',
+  appName: 'Groupie Tracker',
+  webDir: 'dist'
+};
+
+export default config;
